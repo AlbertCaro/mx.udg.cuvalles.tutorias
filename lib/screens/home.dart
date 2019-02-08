1985e455524c0dbee0e389ea9e01ae0f54225482
Alberto Caro Navarro <albertcaronava@gmail.com>
2019-02-08 13:12:08 -0600

    Reestructuración de archivos.

---
+++ lib/screens/home.dart
@@ -0,0 +1,22 @@
+import 'package:flutter/material.dart';
+import 'package:tutorias_app/values/constants.dart';
+
+class HomePage extends StatefulWidget {
+  HomePage({Key key}) : super(key: key);
+
+  @override
+  _HomePageState createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(APP_NAME),
+      ),
+      body: Container(),
+    );
+  }
+}
---
+++ lib/values/constants.dart
@@ -0,0 +1,9 @@
+import 'package:flutter/material.dart';
+
+// Colores
+const PRIMARY_COLOR = Color(0xFF00538f);
+const ACCENT_COLOR = Color(0xFF78b41e);
+const LIST_BACKGROUND_COLOR = Color(0xFFf2f2f2);
+
+// Strings
+const APP_NAME = "Tutorías CUValles";
